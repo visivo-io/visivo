86ddf400b5a393210a7225602696a1962abb22e7
Tim <timothy@overly.me>
2023-09-19 11:25:06 +0100

    Add error handling

--- tests/commands/test_archive.py
+++ tests/commands/test_archive.py
@@ -6,6 +6,6 @@ runner = CliRunner()
 
 
 def test_archive_without_permission():
-    response = runner.invoke(archive, ["-s", "test"])
+    response = runner.invoke(archive, ["-s", "test", "-u", "/tmp"])
     assert "Token not authorized for hos" in response.output
     assert response.exit_code == 1
