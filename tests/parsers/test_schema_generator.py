26e6d1425a757c699e955312ef4faefe6b2b5526
Tim Overly <timothy.overly@gmail.com>
2023-09-21 12:06:06 +0100

    Fix schema generation (#15)

--- tests/parsers/test_schema_generator.py
+++ tests/parsers/test_schema_generator.py
@@ -5,11 +5,11 @@ from visivo.parsers.schema_generator import generate_schema
 
 def test_generate_schema_replaces_unsupported_javascript():
     schema = generate_schema()
-    schema_string = json.dumps(schema)
 
-    assert "?P<ref_name>" in schema_string
+    assert "?P<ref_name>" not in schema
+    assert "?P<column_name>" not in schema
+    assert "?P<query_statement>" not in schema
 
-    schema_string = schema_string.replace("?P<ref_name>", "")
-    tmp = temp_file(name="visivo_schema.json", contents=schema_string)
+    tmp = temp_file(name="visivo_schema.json", contents=schema)
 
     assert tmp.exists()
