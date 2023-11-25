e625274c6a0c3f2f09b7f234c795409d93cf96f0
Tim <timothy@overly.me>
2023-11-25 22:21:40 +0000

    Catch db error and report

--- tests/models/test_target.py
+++ tests/models/test_target.py
@@ -1,14 +1,12 @@
-from visivo.models.target import Target
+from visivo.models.target import Target, TypeEnum
 from pydantic import ValidationError
 from tests.factories.model_factories import TargetFactory
 import pytest
+import click
 
 
 def test_Target_simple_data():
-    data = {
-        "name": "development",
-        "database": "database",
-    }
+    data = {"name": "development", "database": "database"}
     target = Target(**data)
     assert target.name == "development"
 
@@ -21,6 +19,23 @@ def test_Target_missing_data():
 
     assert error["msg"] == "Field required"
     assert error["type"] == "missing"
+
+
+def test_Target_bad_connection():
+    data = {
+        "name": "development",
+        "database": "database",
+        "port": 5434,
+        "type": TypeEnum.postgresql,
+    }
+    target = Target(**data)
+    with pytest.raises(click.ClickException) as exc_info:
+        target.read_sql("query")
+
+    assert (
+        exc_info.value.message
+        == "Error connecting to target 'development'. Ensure the database is running and the connection properties are correct."
+    )
 
 
 def test_Target_password_json():
