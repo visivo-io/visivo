32bac388985dd4f9dbff0104503ea4bb57225dff
jpjsonic <35309169+jpjsonic@users.noreply.github.com>
2023-12-19 10:23:14 -0500

    Updated table & chart docs

--- visivo/models/table.py
+++ visivo/models/table.py
@@ -9,16 +9,19 @@ from typing_extensions import Annotated
 
 class Table(NamedModel, ParentModel):
     """
-    Tables enable you to represent the data aggregated in your trace in a tabular format. 
-    
+    Tables enable you to represent the data aggregated in your trace in a tabular format. Tables are built on the [material react table framework](https://www.material-react-table.com/). 
+
+    ??? note
+
+        We're actively working on improving the table interface and incorporating more features from material react table such as [aggregation & grouping](https://www.material-react-table.com/docs/guides/aggregation-and-grouping). 
     """
     trace: Union[Annotated[str, StringConstraints(pattern=REF_REGEX)], Trace] = Field(
         ...,
-        description="",
+        description="A ref() to a trace or trace defined in line. Data for the table will come from the trace.",
     )
     columns: List[dict] = Field(
         ...,
-        description="",
+        description="A list of dictionaries that contain the keys `header` and `column`. `header` is the title of the column in the table. `column` is the column name from the trace that you want to include in the table."
     )
 
     def child_items(self):
