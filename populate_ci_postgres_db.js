23487b1799b822728563930ff7164f9d2ef0987f
jpjsonic <35309169+jpjsonic@users.noreply.github.com>
2023-12-20 10:14:56 -0500

    Updated working table generation script

--- populate_ci_postgres_db.js
+++ populate_ci_postgres_db.js
@@ -1,36 +1,3 @@
-// const { Client } = require('pg');
-
-// const pgclient = new Client({
-//     host: process.env.POSTGRES_HOST,
-//     port: process.env.POSTGRES_PORT,
-//     user: 'postgres',
-//     password: 'postgres',
-//     database: 'postgres'
-// });
-
-
-// const table = 'CREATE TABLE IF NOT EXISTS test_table(id SERIAL PRIMARY KEY, x INT, y INT)';
-// const text = 'INSERT INTO test_table(x, y) VALUES($1, $2) RETURNING *';
-
-// const records = [
-//     [1, 1], [2, 1], [3, 2], [4, 3], [5, 5], [6, 8]
-// ];
-
-// pgclient.connect();
-
-// pgclient.query(table, (err, res) => {
-//     if (err) throw err
-// });
-
-// records.forEach(record => {
-//     pgclient.query(text, record, (err, res) => {
-//         if (err) throw err;
-//         console.log('Inserted:', res.rows[0]);
-//     });
-// });
-
-
-// pgclient.end();
 const { Client } = require('pg');
 
 const pgclient = new Client({
@@ -43,19 +10,19 @@ const pgclient = new Client({
 
 pgclient.connect();
 
-const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
-const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
-const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+const table = 'CREATE TABLE test_table(id SERIAL PRIMARY KEY, x INT, y INT)'
+const inserts = 'INSERT INTO test_table(x, y) VALUES(1, 1); INSERT INTO test_table(x, y) VALUES(2, 1); INSERT INTO test_table(x, y) VALUES(3, 2); INSERT INTO test_table(x, y) VALUES(4, 3); INSERT INTO test_table(x, y) VALUES(5, 5); INSERT INTO test_table(x, y) VALUES(6, 8);'
+
 
 pgclient.query(table, (err, res) => {
     if (err) throw err
 });
 
-pgclient.query(text, values, (err, res) => {
+pgclient.query(inserts, (err, res) => {
     if (err) throw err
 });
 
-pgclient.query('SELECT * FROM student', (err, res) => {
+pgclient.query('SELECT * FROM test_table', (err, res) => {
     if (err) throw err
     console.log(err, res.rows) // Print the data in student table
     pgclient.end()
