1bf23e7ea0b2d1457adfb21b969aeaa431dd7740
jpjsonic <35309169+jpjsonic@users.noreply.github.com>
2023-12-20 10:06:42 -0500

    Update to create table script

--- populate_ci_postgres_db.js
+++ populate_ci_postgres_db.js
@@ -8,7 +8,6 @@ const pgclient = new Client({
     database: 'postgres'
 });
 
-pgclient.connect();
 
 const table = 'CREATE TABLE IF NOT EXISTS test_table(id SERIAL PRIMARY KEY, x INT, y INT)';
 const text = 'INSERT INTO test_table(x, y) VALUES($1, $2) RETURNING *';
@@ -16,6 +15,12 @@ const text = 'INSERT INTO test_table(x, y) VALUES($1, $2) RETURNING *';
 const records = [
     [1, 1], [2, 1], [3, 2], [4, 3], [5, 5], [6, 8]
 ];
+
+pgclient.connect();
+
+pgclient.query(table, (err, res) => {
+    if (err) throw err
+});
 
 records.forEach(record => {
     pgclient.query(text, record, (err, res) => {
