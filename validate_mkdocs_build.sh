e004cc6d6ce5a909a4c482038c3af0260369565e
jpjsonic <35309169+jpjsonic@users.noreply.github.com>
2024-02-06 22:04:15 -0500

    Fixed check for spelling errors

--- validate_mkdocs_build.sh
+++ validate_mkdocs_build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if grep -q "string_to_search" build_stdout.txt; then
+if grep -q "WARNING -  mkdocs_spellcheck" build_stdout.txt; then
   echo "Spelling errors were found in the docs. Please fix the error or add the words to the mkdocs/known_words.txt"
   exit 1
+else 
+  echo "You spel purtty guod"
 fi
 
