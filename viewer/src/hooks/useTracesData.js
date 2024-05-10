9c0fd7c3cb80eeb4a044bd147bbef2e2ce8dfb9e
Tim Overly <timothy.overly@gmail.com>
2024-05-10 12:54:49 +0100

    Address two issues (#78)

--- viewer/src/hooks/useTracesData.js
+++ viewer/src/hooks/useTracesData.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import FetchTracesQueryContext from "../contexts/FetchTracesQueryContext";
 import { useQuery } from '@tanstack/react-query'
 import { fetchTracesData } from "../queries/tracesData";
