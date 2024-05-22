91d5a64ea0b0849b8197b58535f6fd235181f793
Tim <timothy@overly.me>
2024-05-21 21:48:31 -0400

    Organize the integration project

--- tests/setup/populate_ci_mysql.sql
+++ tests/setup/populate_ci_mysql.sql
@@ -13,3 +13,14 @@ INSERT INTO test_table(x, y) VALUES(3, 2);
 INSERT INTO test_table(x, y) VALUES(4, 3); 
 INSERT INTO test_table(x, y) VALUES(5, 5); 
 INSERT INTO test_table(x, y) VALUES(6, 8);
+
+DROP TABLE IF EXISTS second_test_table;
+
+CREATE TABLE second_test_table(id SERIAL PRIMARY KEY, x INT, y INT);
+
+INSERT INTO second_test_table(x, y) VALUES(1, 2); 
+INSERT INTO second_test_table(x, y) VALUES(2, 2); 
+INSERT INTO second_test_table(x, y) VALUES(3, 4); 
+INSERT INTO second_test_table(x, y) VALUES(4, 6); 
+INSERT INTO second_test_table(x, y) VALUES(5, 10); 
+INSERT INTO second_test_table(x, y) VALUES(6, 16);
--- tests/setup/populate_ci_postgres.sql
+++ tests/setup/populate_ci_postgres.sql
@@ -6,3 +6,12 @@ INSERT INTO test_table(x, y) VALUES(3, 2);
 INSERT INTO test_table(x, y) VALUES(4, 3); 
 INSERT INTO test_table(x, y) VALUES(5, 5); 
 INSERT INTO test_table(x, y) VALUES(6, 8);
+
+CREATE TABLE second_test_table(id SERIAL PRIMARY KEY, x INT, y INT);
+
+INSERT INTO second_test_table(x, y) VALUES(1, 2); 
+INSERT INTO second_test_table(x, y) VALUES(2, 2); 
+INSERT INTO second_test_table(x, y) VALUES(3, 4); 
+INSERT INTO second_test_table(x, y) VALUES(4, 6); 
+INSERT INTO second_test_table(x, y) VALUES(5, 10); 
+INSERT INTO second_test_table(x, y) VALUES(6, 16);
