f9bf17872b09be578cdd648232121bb78e295772
Tim <timothy@overly.me>
2024-06-04 15:16:22 -0400

    Working selection across charts

--- viewer/src/components/items/CohortSelect.js
+++ viewer/src/components/items/CohortSelect.js
@@ -13,17 +13,27 @@ const TraceSelect = ({ onChange, tracesData, isMulti, showLabel, name }) => {
 
     const defaultOptions = useMemo(() => {
         return isMulti ? options : options[0];
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [JSON.stringify(options), isMulti])
 
-    const getOptionValues = (optionArrayOrObject) => {
+    const getValuesFromOptions = (optionArrayOrObject) => {
         if (Array.isArray(optionArrayOrObject)) {
             return optionArrayOrObject.map((optionArray) => optionArray.value)
         } else {
             return [optionArrayOrObject.value]
         }
-
     }
+
+    const getOptionsFromValues = (valueArrayOrString) => {
+        if (Array.isArray(valueArrayOrString)) {
+            return valueArrayOrString.map((valueArray) => {
+                return { value: valueArray, label: valueArray }
+            })
+        } else {
+            return { value: valueArrayOrString, label: valueArrayOrString }
+        }
+    }
+
     const onSelectChange = (selectedOptions) => {
         setSearchParams((previousSearchParams) => {
             const newSearchParams = new URLSearchParams(previousSearchParams.toString())
@@ -31,15 +41,15 @@ const TraceSelect = ({ onChange, tracesData, isMulti, showLabel, name }) => {
             if (newSearchParams.has(name)) {
                 newSearchParams.delete(name)
             }
-            const selectedOptionsValues = getOptionValues(selectedOptions)
-            const defaultOptionsValues = getOptionValues(defaultOptions)
+            const selectedOptionsValues = getValuesFromOptions(selectedOptions)
+            const defaultOptionsValues = getValuesFromOptions(defaultOptions)
             if (selectedOptionsValues.length === defaultOptionsValues.length
                 && defaultOptionsValues.every(dov => selectedOptionsValues.includes(dov))) {
                 return newSearchParams
             }
 
             if (Array.isArray(selectedOptions) && selectedOptions.length !== 0) {
-                newSearchParams.append(name, getOptionValues(selectedOptions))
+                newSearchParams.append(name, getValuesFromOptions(selectedOptions))
             } else if (Array.isArray(selectedOptions) && selectedOptions.length === 0) {
                 newSearchParams.append(name, "NoCohorts")
             } else {
@@ -49,7 +59,7 @@ const TraceSelect = ({ onChange, tracesData, isMulti, showLabel, name }) => {
         })
     }
 
-    const cohortSelection = useMemo(() => {
+    const selectedCohortNames = useMemo(() => {
         if (searchParams.has(name)) {
             return searchParams.getAll(name)
         } else {
@@ -59,12 +69,12 @@ const TraceSelect = ({ onChange, tracesData, isMulti, showLabel, name }) => {
 
     useEffect(() => {
         const newTraceData = {};
-        if (!cohortSelection) {
+        if (!selectedCohortNames) {
             return newTraceData;
         }
         Object.keys(tracesData).forEach((traceName) => {
             Object.keys(tracesData[traceName]).forEach((cohortName) => {
-                if (cohortSelection === cohortName || cohortSelection.includes(cohortName)) {
+                if (selectedCohortNames === cohortName || selectedCohortNames.includes(cohortName)) {
                     if (!newTraceData[traceName]) {
                         newTraceData[traceName] = {}
                     }
@@ -74,7 +84,7 @@ const TraceSelect = ({ onChange, tracesData, isMulti, showLabel, name }) => {
         })
         onChange(newTraceData)
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [JSON.stringify(cohortSelection)]);
+    }, [JSON.stringify(selectedCohortNames)]);
 
     useEffect(() => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -83,7 +93,7 @@ const TraceSelect = ({ onChange, tracesData, isMulti, showLabel, name }) => {
     return (
         <>
             {showLabel && <label htmlFor='traceSelect'>Traces</label>}
-            <Select inputId="traceSelect" options={options} defaultValue={defaultOptions} isMulti={isMulti} onChange={onSelectChange} />
+            <Select inputId="traceSelect" options={options} defaultValue={getOptionsFromValues(selectedCohortNames)} isMulti={isMulti} onChange={onSelectChange} />
         </>
     )
 }
