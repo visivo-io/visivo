240dde1b7b950f6bf9f11603f26601a790302e50
Tim <timothy@overly.me>
2024-06-30 20:25:13 -0400

    Add test doc

--- visivo/models/test.py
+++ visivo/models/test.py
@@ -1,5 +1,25 @@
 from .base.base_model import BaseModel
 
+"""
+Tests allow you to assert on the computed values that are the output of a trace.  The tests are run with the `visivo test` command.
+
+### Example
+``` yaml
+traces:
+    - name: Tested Trace
+      model: ref(model)
+      columns:
+          account_name: account_name
+      props:
+          type: scatter
+          x: column(project_created_at)
+          y: column(project_name)
+      tests:
+        - logic: assert_that(numpy.sum(Tested Trace.props.x)).is_equal_to(7)
+```
+The [assertpy](https://assertpy.github.io/) and [numpy](https://numpy.org/doc/stable/index.html) libraries are available for testing.
+"""
+
 
 class Test(BaseModel):
     logic: str
