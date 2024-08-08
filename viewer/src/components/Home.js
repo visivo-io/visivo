6b5e952f4c38abdca45363af8fa6c3173e90aeb4
Tim Overly <timothy.overly@gmail.com>
2024-08-07 20:14:10 -0400

    Add timestamp to dashboard (#114)

--- viewer/src/components/Home.js
+++ viewer/src/components/Home.js
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Breadcrumbs from './Breadcrumbs';
+import ProjectHistory from './ProjectHistory';
 
 const Home = () => {
   return (
     <div>
       <div className='mx-2'>
-        <Breadcrumbs />
+        <div className="flex flex-row justify-between items-center whitespace-nowrap">
+          <Breadcrumbs />
+          <ProjectHistory />
+        </div>
         <Outlet />
       </div>
     </div>
