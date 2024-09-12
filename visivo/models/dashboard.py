480185092cbbaabeb566f60c2c83af2a48126d63
jpjsonic <jpjsonic@gmail.com>
2024-09-12 12:50:35 -0400

    Immprove row docs

--- visivo/models/dashboard.py
+++ visivo/models/dashboard.py
@@ -10,28 +10,32 @@ from typing import List
 
 class Dashboard(NamedModel, ParentModel):
     """
-    Dashboards are grids where you are able to organize and display `charts`, `tables`, `selectors` and `markdown` from your project.
+    Dashboards are lists of rows. Within the [rows](./row) you are able to organize and display `charts`, `tables`, `selectors` and `markdown` from your project as items.
 
-    The grid is build from `Rows` that can house 1 to n `Items`. `Items` wrap around your charts, tables or markdown and allow you to control the width of your columns within the Row. Here's an example of a dashboard configuration:
-    ``` yaml
-    dashboards:
-      - name: any-name-you-want  #unique name of your dashboard
-        rows:
-          - height: medium
-            items:
-              - width: 2  #widths are evaluated relative to other items in the row
-                table: ref(a-table-name)
-              - width 1  #this chart will be 1/3 of the row
-                chart: ref(a-chart-name)
-          - height: small
-            items:
-              - width: 1
-                markdown: "# Some inline **markdown**"
-              - width: 1
-                chart: ref(another-chart)
-              - width: 2
-                chart: ref(a-third-chart)
-    ```
+    The grid is build from `Rows` that can house 1 to n `Items`. `Items` wrap around your charts, tables or markdown and allow you to control the width of your columns within the Row. 
+    
+    !!! example
+
+        ``` yaml
+        dashboards:
+        - name: any-name-you-want  #unique name of your dashboard
+            rows:
+            - height: compact
+            - height: medium
+                items:
+                - width: 2  #widths are evaluated relative to other items in the row
+                    table: ref(a-table-name)
+                - width 1  #this chart will be 1/3 of the row
+                    chart: ref(a-chart-name)
+            - height: small
+                items:
+                - width: 1
+                    markdown: "# Some inline **markdown**"
+                - width: 1
+                    chart: ref(another-chart)
+                - width: 2
+                    chart: ref(a-third-chart)
+        ```
     """
 
     def child_items(self):
