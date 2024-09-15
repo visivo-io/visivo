751a44bf82bf365b5aee44b59b500a4652ca6af1
Tim Overly <timothy.overly@gmail.com>
2024-09-14 22:56:43 -0400

    Test fix with NoCohorts (#130)
    * Test fix with NoCohorts
    
    * Add no autorange
    
    * Whoops
    
    * Duplicate object
    
    * Make a duplicate of the layout object
    
    * Upgrade and fix tests
    
    * Upgrade and fix tests
    
    * Set version
    
    * Revert file
    
    * replace parse
    
    * try 16
    
    * Try 20

--- viewer/src/components/items/Chart.js
+++ viewer/src/components/items/Chart.js
@@ -6,7 +6,7 @@ import CohortSelect from "./CohortSelect";
 import { traceNamesInData, chartDataFromCohortData } from "../../models/Trace";
 import { useTracesData } from "../../hooks/useTracesData";
 import MenuItem from "../styled/MenuItem";
-import { ItemContainer  } from "./ItemContainer";
+import { ItemContainer } from "./ItemContainer";
 
 const Chart = ({ chart, project, itemWidth, height, width }) => {
     const traceNames = chart.traces.map((trace) => trace.name)
@@ -36,6 +36,8 @@ const Chart = ({ chart, project, itemWidth, height, width }) => {
         setSelectedCohortData(changedSelectedTracesData)
     }
 
+    const layout = structuredClone(chart.layout ? chart.layout : {})
+
     return (
         <ItemContainer onMouseOver={() => setHovering(true)} onMouseOut={() => setHovering(false)}>
             <Menu hovering={hovering && cohortSelectVisible}>
@@ -54,7 +56,7 @@ const Chart = ({ chart, project, itemWidth, height, width }) => {
                 key={`chart_${chart.name}`}
                 data-testid={`chart_${chart.name}`}
                 data={selectedPlotData}
-                layout={{ ...chart.layout, height, width }}
+                layout={{ ...layout, height, width }}
                 useResizeHandler={true}
                 config={{ displayModeBar: false }}
             />
--- viewer/src/components/items/CohortSelect.js
+++ viewer/src/components/items/CohortSelect.js
@@ -3,7 +3,10 @@ import Select from 'react-select'
 import { cohortNamesInData } from '../../models/Trace';
 import { useSearchParams } from "react-router-dom";
 
-const getOptionsFromValues = (valueArrayOrString) => {
+export const getOptionsFromValues = (valueArrayOrString) => {
+    if (!valueArrayOrString) {
+        return null
+    }
     if (Array.isArray(valueArrayOrString)) {
         return valueArrayOrString.map((valueArray) => {
             return { value: valueArray, label: valueArray }
@@ -125,10 +128,12 @@ const CohortSelect = ({
     }, []);
 
     const selectedCohortNames = useMemo(() => {
-        if (searchParams.has(name)) {
+        if (searchParams.has(name) && searchParams.get(name).includes("NoCohorts")) {
+            return null
+        } else if (searchParams.has(name)) {
             return searchParams.get(name).split(",")
         } else if (!defaultOptions) {
-            return ""
+            return null
         } else {
             return Array.isArray(defaultOptions) ? defaultOptions.map((ds) => ds.value) : defaultOptions.value
         }
