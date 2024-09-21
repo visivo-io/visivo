31b9e300fbe8d1bd89fc07ddb21b09fa87f685ab
Tim <timothy@overly.me>
2024-09-21 20:26:58 +0100

    Make it hover

--- viewer/src/components/styled/Error.js
+++ viewer/src/components/styled/Error.js
@@ -1,6 +1,6 @@
 const Error = ({ children }) => {
     return (
-        <div className="flex justify-center items-center p-4 bg-red-100 rounded-lg shadow-md">
+        <div className="fixed top-1 left-1 right-1 z-50 flex justify-center items-center p-4 bg-red-100 rounded-lg shadow-md">
             <p className="text-red-600 font-semibold text-center">
                 {children}
             </p>
