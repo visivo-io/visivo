55da939ccfbaa581250a484f8573f390a4c6d956
jpjsonic <jpjsonic@gmail.com>
2024-09-24 17:19:35 -0400

    Fixes the docs continuing to prefer targets over sources

--- visivo/parsers/schema_generator.py
+++ visivo/parsers/schema_generator.py
@@ -3,7 +3,7 @@ from visivo.models.project import Project
 
 
 def generate_schema():
-    schema = Project.model_json_schema()
+    schema = Project.model_json_schema(by_alias=False)
     schema_string = json.dumps(schema)
     schema_string = schema_string.replace("?P<ref_name>", "")
     schema_string = schema_string.replace("?P<column_name>", "")
