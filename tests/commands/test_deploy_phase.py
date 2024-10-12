79acd840d6329a65f18c16c1b2edbdb15ef11fc1
Tim <timothy@overly.me>
2024-10-12 15:01:58 +0100

    Wrong import make tests better

--- tests/commands/test_deploy_phase.py
+++ tests/commands/test_deploy_phase.py
@@ -12,12 +12,25 @@ def test_deploy_success(requests_mock, capsys):
     project = ProjectFactory()
     create_file_database(url=project.sources[0].url(), output_dir=output_dir)
 
-    tmp = temp_yml_file(dict=json.loads('{"name": "name"}'), name=PROJECT_FILE_NAME)
+    tmp = temp_yml_file(
+        dict=json.loads(project.model_dump_json()), name=PROJECT_FILE_NAME
+    )
     working_dir = os.path.dirname(tmp)
     temp_file(PROFILE_FILE_NAME, "token: value", working_dir + "/.visivo")
+    temp_file("data.json", "{}", output_dir + "/trace")
 
     requests_mock.post(
         "http://host/api/projects/",
+        json={"name": "name", "id": "id", "url": "/url"},
+        status_code=201,
+    )
+    requests_mock.post(
+        "http://host/api/files/",
+        json={"name": "name", "id": "id", "url": "/url"},
+        status_code=201,
+    )
+    requests_mock.post(
+        "http://host/api/traces/",
         json={"name": "name", "id": "id", "url": "/url"},
         status_code=201,
     )
@@ -29,3 +42,6 @@ def test_deploy_success(requests_mock, capsys):
         host="http://host",
     )
     assert "/url" == url
+    captured = capsys.readouterr()
+    assert "Project uploaded" in captured.out
+    assert "Trace 'trace' data uploaded" in captured.out
