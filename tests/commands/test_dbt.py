77c15d60a6622668540a4a4e78ccadc09f338014
Tim <timothy@overly.me>
2024-10-23 23:20:35 +0100

    More stuff

--- tests/commands/test_dbt.py
+++ tests/commands/test_dbt.py
@@ -4,7 +4,7 @@ from visivo.commands.dbt import dbt
 from visivo.parsers.file_names import PROJECT_FILE_NAME
 from visivo.commands.utils import create_file_database
 from click.testing import CliRunner
-from tests.support.utils import temp_yml_file, temp_folder
+from tests.support.utils import temp_file, temp_yml_file, temp_folder
 from tests.factories.model_factories import DbtFactory, ProjectFactory
 
 runner = CliRunner()
@@ -22,11 +22,45 @@ def test_dbt():
         output_dir=working_dir,
     )
     temp_yml_file(
-        dict={"profile_name": {"outputs": {"target_name": {"type": "snowflake"}}}},
+        dict={
+            "profile_name": {
+                "outputs": {
+                    "target_name": {
+                        "type": "snowflake",
+                        "account": "account",
+                        "username": "username",
+                        "password": "password",
+                        "schema": "schema",
+                        "database": "database",
+                        "threads": 8,
+                        "warehouse": "warehouse",
+                        "role": "role",
+                    }
+                }
+            }
+        },
         name="profiles.yml",
         output_dir=working_dir,
     )
-    temp_yml_file(dict={}, name="dbt_project.yml", output_dir=working_dir)
+    temp_yml_file(
+        dict={"target-path": "target"}, name="dbt_project.yml", output_dir=working_dir
+    )
+
+    temp_file(
+        contents=json.dumps(
+            {
+                "nodes": {
+                    "model.visivo_qa.fact_transaction": {
+                        "database": "RAW",
+                        "schema": "salesmart",
+                        "name": "fact_transaction",
+                    }
+                }
+            }
+        ),
+        name="manifest.json",
+        output_dir=f"{working_dir}/target",
+    )
 
     working_dir = os.path.dirname(tmp)
 
@@ -34,9 +68,4 @@ def test_dbt():
 
     assert "Refreshed dbt models and sources" in response.output
     assert response.exit_code == 0
-    assert os.path.exists(f"dist/data/trace/data.json")
-    assert os.path.exists(f"dist/data/error.json")
-    assert os.path.exists(f"dist/data/project_history.json")
-    assert os.path.exists(f"dist/data/project.json")
-    with open("dist/data/project.json") as project_json:
-        assert '"created_at"' in project_json.read()
+    assert os.path.exists(f"{output_dir}/dbt.yml")
--- visivo/commands/dbt_phase.py
+++ visivo/commands/dbt_phase.py
@@ -1,3 +1,47 @@
+def _generate_sources(profiles):
+    from visivo.logging.logger import Logger
+
+    sources = []
+    for profile_name, profile_value in profiles.items():
+        for target_name, target_value in profile_value["outputs"].items():
+            source = target_value
+            source["name"] = f"dbt_{profile_name}_{target_name}"
+            if "schema" in source:
+                source["db_schema"] = source.pop("schema")
+            if "user" in source:
+                source["username"] = source.pop("user")
+            if "type" in source and source["type"] == "snowflake":
+                Logger.instance().info(
+                    f"Configuring threads for snowflake is not supported.  Ignoring."
+                )
+                if "threads" in source:
+                    source.pop("threads")
+            sources.append(source)
+
+    return sources
+
+
+def _generate_models(manifest):
+    models = []
+    if "nodes" in manifest:
+        for path, node in manifest["nodes"].items():
+            if path.startswith("model."):
+                names = []
+                if "database" in node:
+                    names.append(node["database"])
+                if "schema" in node:
+                    names.append(node["schema"])
+                names.append(node["name"])
+                model = {
+                    "name": f"dbt_{node['name']}",
+                    "sql": "select * from " + ".".join(names),
+                }
+
+                models.append(model)
+
+    return models
+
+
 def dbt_phase(working_dir, output_dir):
     from .utils import parse_project_file
 
@@ -7,19 +51,22 @@ def dbt_phase(working_dir, output_dir):
     if dbt_properties:
         from visivo.logging.logger import Logger
 
-        dbt_project_file = f"{working_dir}/dbt_project.yml"
+        dbt_root = working_dir
         if dbt_properties.dbt_project_yml_location:
-            dbt_project_file = f"{working_dir}/{dbt_properties.dbt_project_yml_location}/dbt_project.yml"
+            dbt_root = f"{working_dir}/{dbt_properties.dbt_project_yml_location}"
+        dbt_project_file = f"{dbt_root}/dbt_project.yml"
+
         profiles_file = f"{working_dir}/profiles.yml"
         if dbt_properties.profiles_yml_location:
             profiles_file = (
                 f"{working_dir}/{dbt_properties.profiles_yml_location}/profiles.yml"
             )
 
-        Logger.instance().debug(f"dbt_project_file: {dbt_project_file}")
-        Logger.instance().debug(f"profiles_file: {profiles_file}")
+        Logger.instance().debug(f"Found dbt_project file: {dbt_project_file}")
+        Logger.instance().debug(f"Found profiles file: {profiles_file}")
 
         import yaml
+        import json
 
         with open(dbt_project_file, "r") as file:
             dbt_project = yaml.safe_load(file)
@@ -27,28 +74,19 @@ def dbt_phase(working_dir, output_dir):
         with open(profiles_file, "r") as file:
             profiles = yaml.safe_load(file)
 
-        sources = []
-        for profile_name, profile_value in profiles.items():
-            for target_name, target_value in profile_value["outputs"].items():
-                source = target_value
-                source["name"] = f"dbt_{profile_name}_{target_name}"
-                if "schema" in source:
-                    source["db_schema"] = source.pop("schema")
-                if "user" in source:
-                    source["username"] = source.pop("user")
-                if "type" in source and source["type"] == "snowflake":
-                    Logger.instance().info(
-                        f"Configuring threads for snowflake is not supported.  Ignoring."
-                    )
-                    if "threads" in source:
-                        source.pop("threads")
-                sources.append(source)
+        with open(
+            f"{dbt_root}/{dbt_project['target-path']}/manifest.json", "r"
+        ) as file:
+            manifest = json.load(file)
+
+        sources = _generate_sources(profiles)
+        models = _generate_models(manifest)
 
         output_file = f"{output_dir}/dbt.yml"
         if dbt_properties.output_file:
             output_file = f"{working_dir}/{dbt_properties.output_file}"
 
         with open(output_file, "w") as file:
-            yaml.dump({"sources": sources}, file)
+            yaml.dump({"sources": sources, "models": models}, file)
 
         Logger.instance().info(f"Refreshed dbt models and sources.")
