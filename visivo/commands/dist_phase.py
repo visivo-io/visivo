c72ad952a3be47a8f279473d358035073564908c
Tim <timothy@overly.me>
2025-03-27 07:51:50 -0400

    Remove dist files

--- visivo/commands/dist_phase.py
+++ visivo/commands/dist_phase.py
@@ -36,22 +36,22 @@ def dist_phase(output_dir, dist_dir):
         with open(f"{dist_dir}/_redirects", "w") as f:
             f.write("/*    /index.html   200")
 
-        with open(f"{dist_dir}/data/error.json", "w") as f:
-            f.write(json.dumps({}))
-        with open(f"{dist_dir}/data/project_history.json", "w") as f:
-            f.write(json.dumps([{"created_at": created_at, "id": "id"}]))
+        # with open(f"{dist_dir}/data/error.json", "w") as f:
+        #     f.write(json.dumps({}))
+        # with open(f"{dist_dir}/data/project_history.json", "w") as f:
+        #     f.write(json.dumps([{"created_at": created_at, "id": "id"}]))
 
-        trace_dirs = glob(f"{output_dir}/traces/*/", recursive=True)
-        for trace_dir in trace_dirs:
-            trace_name = os.path.basename(os.path.normpath(trace_dir))
-            if os.path.exists(f"{output_dir}/traces/{trace_name}/data.json"):
-                os.makedirs(f"{dist_dir}/data/{trace_name}", exist_ok=True)
-                shutil.copyfile(
-                    f"{output_dir}/traces/{trace_name}/data.json",
-                    f"{dist_dir}/data/{trace_name}/data.json",
-                )
+        # trace_dirs = glob(f"{output_dir}/traces/*/", recursive=True)
+        # for trace_dir in trace_dirs:
+        #     trace_name = os.path.basename(os.path.normpath(trace_dir))
+        #     if os.path.exists(f"{output_dir}/traces/{trace_name}/data.json"):
+        #         os.makedirs(f"{dist_dir}/data/{trace_name}", exist_ok=True)
+        #         shutil.copyfile(
+        #             f"{output_dir}/traces/{trace_name}/data.json",
+        #             f"{dist_dir}/data/{trace_name}/data.json",
+        #         )
 
-        shutil.copytree(DIST_PATH, dist_dir, dirs_exist_ok=True)
+        # shutil.copytree(DIST_PATH, dist_dir, dirs_exist_ok=True)
     except Exception as e:
         Logger.instance().error(
             f"Error creating dist. Try running `visivo run` to ensure your project is up to date."
