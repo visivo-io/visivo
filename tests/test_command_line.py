34027fabca725439783ebf61350e68ce66860566
Tim <timothy@overly.me>
2025-05-31 17:49:23 -0400

    Add report link

--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -12,3 +12,20 @@ def test_CommandLine_env_load_exists():
 def test_CommandLine_env_load_does_not_exists():
     load_env(".env.no-exist")
     assert os.getenv("OTHER_VALUE") == None
+
+
+def test_CommandLine_error_reporting():
+    from visivo.command_line import print_issue_url
+    from io import StringIO
+    import sys
+
+    stdout = StringIO()
+    sys.stdout = stdout
+
+    print_issue_url()
+
+    sys.stdout = sys.__stdout__
+    output = stdout.getvalue()
+
+    assert "Click here to report this issue" in output
+    assert "https://github.com/visivo-io/visivo/issues/new" in output
