841cfeace3696460cf50d6db5d3d2571f749c979
Tim Overly <timothy.overly@gmail.com>
2025-06-03 20:09:19 +0100

    Update release command to fix imports (#249)
    * Update
    
    * Update release command

--- visivo/generate_project_schema_json.py
+++ visivo/generate_project_schema_json.py
@@ -1,8 +1,7 @@
-import os
-import json
 from visivo.utils import SCHEMA_FILE
 from visivo.logging.logger import Logger
 from importlib import reload, import_module
+from visivo.parsers.schema_generator import generate_schema
 
 """
 Generates the project schema json file for the current version of the CLI. 
@@ -12,15 +11,8 @@ validate schema client side and to power flask app api endpoints.
 """
 
 
-def generate_schema():
+def reload_and_generate_schema():
     # Store original environment state
-
-    if os.getenv("EXCLUDE_TRACE_PROPS") != "False":
-        raise Exception("EXCLUDE_TRACE_PROPS env_var must be set to False to generate the schema")
-
-    # Import and reload all necessary modules in the correct order
-    trace_props_module = import_module("visivo.models.trace_props.fields")
-    reload(trace_props_module)
 
     trace_module = import_module("visivo.models.trace")
     reload(trace_module)
@@ -28,19 +20,7 @@ def generate_schema():
     project_module = import_module("visivo.models.project")
     reload(project_module)
 
-    Project = project_module.Project
-
-    # Generate schema with complete properties
-    schema = Project.model_json_schema(by_alias=False)
-    schema["$schema"] = "https://json-schema.org/draft/2020-12/schema"
-    schema_string = json.dumps(schema)
-
-    schema_string = schema_string.replace('\\"', "")
-    schema_string = schema_string.replace("?P<ref_name>", "")
-    schema_string = schema_string.replace("?P<column_name>", "")
-    schema_string = schema_string.replace("?P<query_statement>", "")
-    schema_string = schema_string.replace("?P<query_string>", "")
-
+    schema_string = generate_schema()
     if "?P<" in schema_string:
         raise Exception(
             f"Schema json contains '?P<': this means that the schema is likely not JS compatible and needs a new find and replace like the ones above this line"
@@ -61,7 +41,7 @@ def write_schema_json() -> None:
     try:
         Logger.instance().info(f"Generating schema file: {SCHEMA_FILE}...")
 
-        schema = generate_schema()
+        schema = reload_and_generate_schema()
         with open(SCHEMA_FILE, "w") as f:
             f.write(schema)
         Logger.instance().info(f"Schema file written to: {SCHEMA_FILE}")
