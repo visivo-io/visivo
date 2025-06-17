9d72aea2c638b85e7ff7e20c321fc9d8620ab698
jpjsonic <jpjsonic@gmail.com>
2025-06-17 06:45:30 -0400

    Fixing issues in tests and code

--- tests/models/models/test_local_merge_model.py
+++ tests/models/models/test_local_merge_model.py
@@ -22,8 +22,13 @@ def test_insert_dependent_models_successfully_inserts_to_duckdb(mocker):
     output_dir = temp_folder()
     os.makedirs(output_dir, exist_ok=True)
 
-    data = [["tom", 10, 1, 1], ["nick", 15, 2, 2], ["juli", 14, 3, 3]]
-    dataframe = pl.DataFrame(data, columns=["name", "age", "id", "external_id"])
+    data = {
+        "name": ["tom", "nick", "juli"],
+        "age": [10, 15, 14],
+        "id": [1, 2, 3],
+        "external_id": [1, 2, 3],
+    }
+    dataframe = pl.DataFrame(data)
     mocker.patch(
         "visivo.models.sources.postgresql_source.PostgresqlSource.read_sql",
         return_value=dataframe,
