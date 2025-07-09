afe519fda6e534463902febc770e4571276d9985
Tim <timothy@overly.me>
2025-07-09 12:44:53 -0400

    Add version

---
+++ test_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Simple test to verify the install script syntax and help functionality
+echo "Testing install.sh script..."
+
+# Test help flag
+echo "Testing --help flag:"
+bash /Users/tgsoverly/code/visivo/install.sh --help
+
+echo ""
+echo "Test completed - if you see the help message above, the script syntax is correct!"
