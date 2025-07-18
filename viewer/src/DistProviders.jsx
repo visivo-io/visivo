e287d83dfe7f76c2bc99eccf62a14d15da1a1acc
jpjsonic <jpjsonic@gmail.com>
2025-07-18 12:39:33 -0400

    We dont need to mess with the distproviders

--- viewer/src/DistProviders.jsx
+++ viewer/src/DistProviders.jsx
@@ -6,7 +6,6 @@ import { QueryProvider } from './contexts/QueryContext';
 import { fetchTracesQuery } from './queries/traces';
 import { fetchDashboardQuery } from './queries/dashboards';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { StoreProvider } from './StoreProvider';
 
 const queryClient = new QueryClient();
 
@@ -14,12 +13,10 @@ export default function DistProviders() {
   return (
     <QueryClientProvider client={queryClient}>
       <QueryProvider value={{ fetchTracesQuery, fetchDashboardQuery }}>
-        <StoreProvider>
-          <RouterProvider
-            router={DistRouter}
-            future={futureFlags}
-          />
-        </StoreProvider>
+        <RouterProvider
+          router={DistRouter}
+          future={futureFlags}
+        />
       </QueryProvider>
     </QueryClientProvider>
   );
